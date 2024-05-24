cc80bc8d821feffb37515e58a9e8433dca87cfd6
Sergey <helloimclinker@gmail.com>
2024-05-24 19:51:20 +0700

    About activity structure is done FoundRecipesActivity is done

--- app/src/main/java/com/example/kitchemate/activity/FoundRecipesActivity.java
+++ app/src/main/java/com/example/kitchemate/activity/FoundRecipesActivity.java
@@ -4,27 +4,41 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.example.kitchemate.R;
 import com.example.kitchemate.adapter.RecipeAdapter;
+import com.example.kitchemate.api.ApiClient;
+import com.example.kitchemate.api.ApiService;
+import com.example.kitchemate.model.Recipe;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class FoundRecipesActivity extends AppCompatActivity {
 
     private ArrayList<String> ingredients;
     private RecipeAdapter recipeAdapter;
     private ListView recipeListView;
-    private ArrayList<String> foundRecipeNames;
+    private ArrayList<Recipe> foundRecipes = new ArrayList<Recipe>();
+    private ApiService apiService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.found_recipes_activity);
-
+        foundRecipes.clear();
         Toolbar toolbar = findViewById(R.id.foundRecipesToolbar);
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
@@ -33,25 +47,50 @@ public class FoundRecipesActivity extends AppCompatActivity {
         }
 
         ingredients = getIntent().getStringArrayListExtra("ingredients");
+        apiService = ApiClient.getClient().create(ApiService.class);
 
-        /*Pair<ArrayList<Recipe>, ArrayList<String>> p = DBHelper.findRecipesByIngredients(this, ingredients);
-
-        ArrayList<Recipe> foundRecipes = p.first;
-        foundRecipeNames = p.second;
         recipeListView = findViewById(R.id.foundRecipesListView);
         recipeAdapter = new RecipeAdapter(this, R.layout.recipe_list_item, foundRecipes);
         recipeListView.setAdapter(recipeAdapter);
+        fetchRecipes(ingredients);
 
         this.recipeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String selectedRecipe = foundRecipeNames.get(position);
+                Recipe selectedRecipe = foundRecipes.get(position);
                 Intent intent = new Intent(FoundRecipesActivity.this, RecipeDetailsActivity.class);
-                intent.putExtra("recipeName", selectedRecipe);
+                intent.putExtra("recipeName", selectedRecipe.getName());
+                intent.putExtra("recipeId", selectedRecipe.getId());
+                intent.putExtra("recipeIngr", selectedRecipe.getIngredients());
+                intent.putExtra("recipeHowTo", selectedRecipe.getHowTo());
+                intent.putExtra("recipeImgP", selectedRecipe.getImagePath());
                 startActivity(intent);
-
             }
-        });*/
+        });
+    }
+
+    private void fetchRecipes(ArrayList<String> ingredients) {
+
+        for (String ingredient : ingredients) {
+            Call<List<Recipe>> call = apiService.searchRecipes(ingredient);
+            call.enqueue(new Callback<List<Recipe>>() {
+                @Override
+                public void onResponse(Call<List<Recipe>> call, Response<List<Recipe>> response) {
+                    if (response.isSuccessful() && response.body() != null) {
+                        foundRecipes.addAll(response.body());
+                        Collections.shuffle(foundRecipes);
+                        recipeAdapter.notifyDataSetChanged();
+                    } else {
+                        Log.e("FAILURE", "Failed to fetch recipes");
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<List<Recipe>> call, Throwable t) {
+                    Log.e("FAILURE", "Failed to fetch recipes");
+                }
+            });
+        }
     }
 
     @Override
--- app/src/main/java/com/example/kitchemate/fragment/AboutFragment.java
+++ app/src/main/java/com/example/kitchemate/fragment/AboutFragment.java
@@ -2,6 +2,10 @@ package com.example.kitchemate.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -10,51 +14,23 @@ import android.view.ViewGroup;
 
 import com.example.kitchemate.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AboutFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class AboutFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public AboutFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AboutFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AboutFragment newInstance(String param1, String param2) {
-        AboutFragment fragment = new AboutFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    @Override
+    public void onViewCreated(@NonNull View view, @NonNull Bundle savedInstanceState) {
+        Toolbar toolbar = getView().findViewById(R.id.toolbarRecipe1);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+        ActionBar actionBar = ((AppCompatActivity)getActivity()).getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        actionBar.setTitle(R.string.aboutTitle);
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
         setRetainInstance(true);
     }
 
